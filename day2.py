862d4e982947c4c81f99115752a47cf0c4d6ee1b
DoubleZed <zintis.zilbers@gmail.com>
2025-12-02 16:42:06 +0100

    Added Part2

--- day2.py
+++ day2.py
@@ -6,7 +6,6 @@ inputPath = os.path.join(os.getcwd(),"input",inputfile)
 inputarray = [x for x in open(inputPath).read().split(",")]
 
 def part1 (inputarr):
-    invalidcount = 0
     invalidsum = 0
     for idrange in inputarr:
         splitted = idrange.split("-")
@@ -15,9 +14,28 @@ def part1 (inputarr):
                 halfid = str(id)
                 leftid, rightid = halfid[:len(halfid)//2], halfid[len(halfid)//2:]
                 if leftid == rightid:
-                    invalidcount += 1
-                    invalidsum = invalidsum + int(halfid)
+                    invalidsum =+ int(halfid)
     return (invalidsum)
 
-print(part1(inputarray))              
+#print(part1(inputarray))              
 
+def part2 (inputarr):
+    invalidsum = 0
+    for idrange in inputarr:
+        splitted = idrange.split("-")
+        for id in range(int(splitted[0]),int(splitted[1])+1):
+            id = str(id)
+            for i in range (1,len(id)):
+                candidate = id[:i]
+                repeated = ""
+                while len(repeated) < len(id):
+                    repeated += candidate              
+                if repeated == id:
+                    match = True # Need to only count once in cases where all digits are the same (11111,888888)
+                    if match:
+                        invalidsum += int(repeated)
+                    break
+            
+    return (invalidsum)
+
+print(part2(inputarray))   
