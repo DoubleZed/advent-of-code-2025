83f4d0159b5585250b755e7e085faeb5ba278d3f
DoubleZed <zintis.zilbers@gmail.com>
2025-12-04 10:11:24 +0100

    Added part 2 of day 3

--- day3.py
+++ day3.py
@@ -17,4 +17,22 @@ def part1 (inputarr):
         pairsum += largepair
     return pairsum
 
+def part2 (inputarr):
+    resultsum = 0
+    for bank in inputarr:
+        result = []
+        battsleft = 12
+        i = 0
+        while battsleft > 0:
+            end = len(bank) - battsleft + 1
+            largest = max(bank[i:end])
+            pos = bank.index(largest, i, end)
+            result.append(largest)
+            battsleft -= 1
+            i = pos + 1
+        bankresult = int(''.join(map(str, result)))
+        resultsum += bankresult
+    return resultsum
+
 print(part1(inputarray))
+print(part2(inputarray))
